c619b49e9fca91470b4864255ed114c3a65d109c
Keyoshi083 <gt.chicken1@gmail.com>
2022-06-26 21:17:00 +0900

    WIP:ESLintの動作がやや怪しい

--- src/world.ts
+++ src/world.ts
@@ -1,13 +1,13 @@
-export default class World{
-  message:string;
- 
-  constructor(message:string){
-    this.message=message;
+export default class World {
+  message: number;
+
+  constructor(message: string) {
+    this.message = message;
   }
 
-  public sayHello(elem:HTMLElement|null){
-    if(elem){
-      elem.innerText=this.message;
+  public sayHello(elem: HTMLElement | null) {
+    if (elem) {
+      elem.innerText = this.message;
     }
   }
 }
